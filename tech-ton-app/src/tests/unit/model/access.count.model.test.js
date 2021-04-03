196baaa3ec86bbfe179f52032513be33e53c7c00
Daniel Alves <daniel.aguiar@zup.com.br>
2021-04-02 22:21:37 -0300

    feat: Implements unit tests
    Signed-off-by: Daniel Alves

--- tech-ton-app/src/tests/unit/model/access.count.model.test.js
+++ tech-ton-app/src/tests/unit/model/access.count.model.test.js
@@ -1,7 +1,5 @@
 const AccessCountModel = require("../../../main/model/access.count.model");
-const accessCount = require("../../mock/access-count.json");
 const httpMocks = require("node-mocks-http");
-const countapi = require('countapi-js');
 
 jest.mock("../../../main/model/access.count.model");
 
@@ -15,11 +13,9 @@ describe("AccessCountModel.incrementAccessCount", () => {
     it("Should have a incrementAccessCount function", () =>{
         expect(typeof AccessCountModel.incrementAccessCount).toBe("function");
     });
-    it("Should return 201 and increment access count", () =>{
-
-    });
-    it("Should handle 'Could not increment access count' and return 400", () =>{
-
+    it("Should increment access count", async () =>{
+        await AccessCountModel.incrementAccessCount(req,res);
+        expect(res.statusCode).toBe(200);
     });
 })
 
@@ -27,10 +23,8 @@ describe("AccessCountModel.getAccessCount", () => {
     it("Should have a incrementAgetAccessCountccessCount function", () =>{
         expect(typeof AccessCountModel.getAccessCount).toBe("function");
     });
-    it("Should return 201 and get access count", () =>{
-
-    });
-    it("Should handle 'Could not get access count' and return 400", () =>{
-
+    it("Should return 201 and get access count", async () =>{
+        await AccessCountModel.getAccessCount(req, res);
+        expect(res.statusCode).toBe(200);
     });
 })
--- tech-ton-app/src/tests/unit/model/users.model.test.js
+++ tech-ton-app/src/tests/unit/model/users.model.test.js
@@ -1,10 +1,7 @@
 const UserModel = require("../../../main/model/users.model");
 const httpMocks = require("node-mocks-http");
 const createUser = require("../../mock/create-user.json");
-const createUserNameNotString = require("../../mock/create-user-name-not-string.json");
-const createUserUserIdNotString = require("../../mock/create-user-user-id-not-string.json");
-
-jest.mock("../../../main/model/users.model");
+const createUserInvalid = require("../../mock/create-user-invalid.json");
 
 let req, res
 const userId = "896fe8db-346e-4cbe-98b8-ee837872acda";
@@ -17,17 +14,23 @@ describe("UserModel.createUser", () => {
     it("Should have a createUser function", () =>{
         expect(typeof UserModel.createUser).toBe("function");
     });
-    it("Should return 201 and create user", () =>{
-
+    it("Should return 201 and create user", async () =>{
+        req.body = createUser
+        await UserModel.createUser(req,res);
+        expect(res.statusCode).toBe(201);
+        expect(res._getJSONData()).toStrictEqual(createUser);
+        expect(res._isEndCalled()).toBeTruthy();
     });
-    it("Should handle 'userId must be a string' and return 400", () =>{
-
+    it("Should return 400 with invalid user", async () =>{
+        req.body = createUserInvalid
+        await UserModel.createUser(req,res);
+        expect(res.statusCode).toBe(400);
+        expect(res._isEndCalled()).toBeTruthy();
     });
-    it("Should handle 'name must be a string' and return 400", () =>{
-
-    });
-    it("Should handle 'could not create user' and return 400", () =>{
-
+    it("Should handle 'could not create user' and return 400", async () =>{
+        await UserModel.createUser(req,res);
+        expect(res.statusCode).toBe(400);
+        expect(res._isEndCalled()).toBeTruthy();
     });
 })
 
@@ -35,13 +38,22 @@ describe("UserModel.getUser", () => {
     it("Should have a getUser function", () =>{
         expect(typeof UserModel.getUser).toBe("function");
     });
-    it("Should return 200 and get user", () =>{
-
+    it("Should return 200 and get user", async () =>{
+        req.params.userId = "896fe8db-346e-4cbe-98b8-ee837872acda";
+        await UserModel.getUser(req, res);
+        expect(res.statusCode).toBe(200);
+        expect(res._getJSONData()).toStrictEqual(createUser);
+        expect(res._isEndCalled()).toBeTruthy();
     });
-    it("Should handle 'user not found' and return 404", () =>{
-
+    it("Should handle 'user not found' and return 404", async () =>{
+        req.params.userId = "1";
+        await UserModel.getUser(req, res);
+        expect(res.statusCode).toBe(404);
+        expect(res._isEndCalled()).toBeTruthy();
     });
-    it("Should handle 'could not create user' and return 400", () =>{
-
+    it("Should handle 'could not create user' and return 400", async () =>{
+        await UserModel.getUser(req, res);
+        expect(res.statusCode).toBe(400);
+        expect(res._isEndCalled()).toBeTruthy();
     });
 })
